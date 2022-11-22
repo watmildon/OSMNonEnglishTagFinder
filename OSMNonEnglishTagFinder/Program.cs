9a4b0342c589fbcdf389cd6a47bdc45976ffbaf5
Matt W <matthew.whilden@gmail.com>
2022-11-22 13:59:38 -0800

    Add project files.

---
+++ OSMNonEnglishTagFinder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+client.DefaultRequestHeaders.Add("User-Agent", "watmildon non english tag finder");
+await ProcessTagInfoAsync(client);
+        
+
+static async Task ProcessTagInfoAsync(HttpClient client)
+{
+    var tagCounts = new Dictionary<string, int>();
+    var lines = File.ReadAllLines(@"..\..\..\WordList.tsv");
+
+    // Get counts for tags skipping ones already in the set (the translations for various languages may collide)
+    // print non zero values
+    // The indexing looks weird becuase I wanted to flip the rows and columns to translated terms
+    // would stay together in the final output.
+    for (int i = 0; i < 5; i++)
+    {
+        foreach (var line in lines)
+        {
+            var keys = line.Split("\t");
+
+            if (!tagCounts.ContainsKey(keys[i]))
+            {
+                await using Stream stream = await client.GetStreamAsync("https://taginfo.openstreetmap.org/api/4/key/" + $"stats?key={keys[i]}");
+                var tagInfo = await JsonSerializer.DeserializeAsync<TagInfoData>(stream);
+                tagCounts.Add(keys[i], tagInfo.data[0].count);
+                if (tagInfo.data[0].count != 0)
+                {
+                    Console.WriteLine($"{keys[i]},{tagInfo.data[0].count}");
+                }
+            }            
+        }
+    }
+}
+
---
+++ OSMNonEnglishTagFinder/TagInfoData.cs
@@ -0,0 +1,16 @@
+﻿
+public class TagInfoData
+{
+    public string url { get; set; }
+    public DateTime data_until { get; set; }
+    public int total { get; set; }
+    public Datum[] data { get; set; }
+}
+
+public class Datum
+{
+    public string type { get; set; }
+    public int count { get; set; }
+    public float count_fraction { get; set; }
+    public int values { get; set; }
+}
